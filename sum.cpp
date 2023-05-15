f51b6125fbb974425adf603f364970385685a4d0
Pawan <pawanptl34@gmail.com>
2023-05-15 12:53:24 +0530

    initial commit

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, result;
+    cout << "Enter first number \n";
+    cin >> a;
+    cout << "\nEnter secound number \n";
+    cin >> b;
+
+    result = a + b;
+    cout << "Sum of a and b is = " << result;
+
+    return 0;
+}
---
+++ variable.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a; // variable declaration
+    a = 6; // variable definition
+
+    int b = 10;
+    cout << "Value of a is " << a
+         << "\nValue of b is " << b;
+    return 0;
+}
