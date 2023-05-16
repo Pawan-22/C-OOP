8225e732854795965838371d26d3f089893146e9
Pawan <pawanptl34@gmail.com>
2023-05-16 21:56:34 +0530

    data types

---
+++ data_type.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"DATA TYPES";
+    int num = 5;
+    float pi = 3.14;
+    char c = 'C';
+    bool is_true = true;
+    bool is_false = false;
+
+    cout<<"\nInteger is "<<num;
+    cout<<"\n Float is "<<pi;
+    cout<<"\n Character is "<<c;
+    cout<<"\n Bool is "<<is_true; // 1 for true
+    cout<<"\n Bool is "<<is_false; // 0 for false
+    return 0;
+    
+}
