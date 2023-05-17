b837aac425e5c50c39acbe043965016c2968c219
Pawan <pawanptl34@gmail.com>
2023-05-17 17:20:55 +0530

    header files

---
+++ header_files.cpp
@@ -0,0 +1,15 @@
+// 2 types of header files
+
+// System header files.(comes with compiler)
+#include <iostream>
+// User defined header files
+// #include"this.h" // this.h should be present in current directory.
+
+using namespace std;
+
+int main(){
+ 
+ cout<<"HEADER FILES"<<endl;
+
+ return 0;
+}
