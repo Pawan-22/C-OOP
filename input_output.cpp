9841320e2058c03c56ae83264d2c14e09676e44f
Pawan <pawanptl34@gmail.com>
2023-05-17 17:06:08 +0530

    I/O

---
+++ input_output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+ int num1, num2;
+ cout<<"Enter num1\n";// << is insertion operator
+ cin>>num1; // >> is extraction operator
+
+ cout<<"Enter num2\n";
+ cin>>num2;
+
+ cout<<"Value of num1 is "<<num1;
+ cout<<"\nValue of num2 is "<<num2;
+
+ return 0;
+}
