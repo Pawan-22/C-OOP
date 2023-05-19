55825c282aca20cda26058e62bc858d886d5324c
Pawan <pawanptl34@gmail.com>
2023-05-19 19:27:14 +0530

    if else

---
+++ if_else.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age;
+    cout<<"Enter your age to check your elgibility for  Driving License!"<<endl;
+    cin>>age;
+
+    // if(age<18){
+    //     cout<<"Not Eligible";
+    // }
+    // else if(age==18){
+    //     cout<<"Almost there!";
+    // }
+
+    // else{
+    //     cout<<"You are eligible";
+    // }
+// ternary operator
+age<18? cout<<"Not elgibile" : cout<<"Elgibile";
+    return 0;
+}
